975f5dbea3923022ce0b0ad24102acc53643a873
KOTIPALLI PRAVEEN KUMAR <2200032132@kluniversity.in>
2025-10-22 19:26:12 +0530

    changec ui

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -128,7 +128,8 @@ function LoginPage() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
               </svg>
             </div>
-            <h3 className="feature-title">Private & Secure</h3>
+            <h3 className="feature-title">Private & Secure (not yet)</h3>
+            
             <p className="feature-description">Your notes stay yours with end-to-end encryption and privacy-first design</p>
           </div>
         </div>
